7df91628a9a1faaaab901ebc208d9bed8a0736d8
anruky <anruky@gmail.com>
2018-01-28 21:54:29 +0800

    express+mongoose

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
 
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+
+const Dishes = require('./models/dishes');
+
+// Connection URL
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
 
 
 var app = express();
