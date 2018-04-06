146966c62f8b94b15b5c79363fae07cf134fb68f
anruky <anruky@gmail.com>
2018-04-06 11:50:00 +0800

    mongoose population -store object ID instead of author name in dishes

--- models/dishes.js
+++ models/dishes.js
@@ -14,14 +14,13 @@ var commentSchema = new Schema({
         type: String,
         required: true
     },
-    author:  {
-        type: String,
-        required: true
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
     }
 }, {
     timestamps: true
 });
-
 
 var dishSchema = new Schema({
     name: {
--- models/user.js
+++ models/user.js
@@ -3,14 +3,25 @@ var Schema = mongoose.Schema;
 
 var passportLocalMongoose = require('passport-local-mongoose');
 
+
 var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
     admin:   {
         type: Boolean,
         default: false
     }
 });
 
-User.plugin(passportLocalMongoose);
+//username/password
+
+User.plugin(passportLocalMongoose); 
 
 
 module.exports = mongoose.model('User', User);
